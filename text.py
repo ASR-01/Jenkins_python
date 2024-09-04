f4df2efe5071308ed3d08c83120c76bcedbbb9d7
ASR-01 <138866897+ASR-01@users.noreply.github.com>
2024-09-04 10:57:44 +0530

    Update text.py

--- text.py
+++ text.py
@@ -1,1 +1,1 @@
-prin("Hello Aditya Singh Rawat World Richest Man Alive and is also A very good Husband and a very good human")
+print("Hello Aditya Singh Rawat World Richest Man Alive and is also A very good Husband and a very good human")
